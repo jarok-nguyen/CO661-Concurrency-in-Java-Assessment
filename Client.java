cf0b55c7781acdae80bb1f22e77b9d5dee81ba40
nl253 <nl253@kent.ac.uk>
2018-10-15 12:44:31 +0100

    Add comments, fix comparison of files

--- Client.java
+++ Client.java
@@ -2,35 +2,47 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.lang.System.setProperty;
 import static java.text.MessageFormat.*;
 
 /**
+ *
+ * TODO add comments.
+ *
  * @author nl253
  */
 
-public class Client {
+public class Client extends Thread {
 
     private static final int ASCII_LOWER_BOUND = 33;
     private static final int ASCII_UPPER_BOUND = 126;
     private static final int NO_ACTIONS = 20;
-    private static int nextId = 0;
+    private static final int NO_CLIENTS = 20;
+    private static AtomicInteger nextId = new AtomicInteger(0);
 
-    public Client(final FileServer fileServer) {
-        final int id = nextId;
-        nextId++;
-        final Logger log = Logger.getAnonymousLogger();
-        log.info("client no " + id + " started");
+    private final Logger log = Logger.getAnonymousLogger();
+    private final int id;
+    private final Server server;
+
+    @override
+    public void run() {
         for (int i = 0; i < NO_ACTIONS; i++) {
             final boolean write = ThreadLocalRandom.current().nextBoolean();
-            final String fileName = randFileName(new ArrayList<>(fileServer.availableFiles()));
+            final String fileName = randFileName(new ArrayList<>(server.availableFiles()));
             log.info(format("client no {0} about to perform {1}{2} action, more specifically {3} on file \"{4}\"", id, i, (i == 1) ? "st" : ((i == 2) ? "nd" : ((i == 3) ? "rd" : "st")), write ? "write" : "read", fileName));
-            fileServer.open(fileName, write ? Mode.READWRITEABLE : Mode.READABLE);
+            server.open(fileName, write ? Mode.READWRITEABLE : Mode.READABLE);
         }
+    }
 
+    public Client(final Server fileServer) {
+        id = nextId.incrementAndGet();
+        server = fileServer;
+        log.info("client no " + id + " created");
     }
 
     private String randFileName(final List<String> allFiles) {
@@ -42,5 +54,17 @@ public class Client {
                 .mapToObj(i -> String.valueOf(ASCII_LOWER_BOUND + ThreadLocalRandom.current().nextInt(ASCII_UPPER_BOUND - ASCII_LOWER_BOUND)))
                 .collect(Collectors.joining())
                 .toString();
+    }
+
+    public static void main(final String... args) {
+        setProperty("java.util.logging.SimpleFormatter.format", "%1$tH:%1$tM:%1$tS %4$-6s %2$s %5$s%6$s%n");
+        final MyFileServer server = new MyFileServer();
+        final List<Client> clients = new LinkedList<>();
+        for (int i = 0; i < NO_CLIENTS; i++) {
+            final Client c = new Client(server);
+            clients.append(c);
+            c.start();
+        }
+        clients.forEach(Client::join);
     }
 }
--- MyFileServer.java
+++ MyFileServer.java
@@ -7,6 +7,32 @@ import java.util.stream.Collectors;
 import static java.text.MessageFormat.format;
 
 /**
+ * This implementation of `FileServer` ensures fairness and avoids races. It does so in the following ways.
+ * A `ReentrantLock` AND a `Semaphore` is created for every file.
+ *
+ * See `files`, there is always a single lock and a single semaphore for the ith file stored in locks in the ith index and semaphores in the ith index respectively.
+ *
+ * There are essentially two nested "critical sections" with the "writing section" being inside the "reading section". 
+ * To enter the "reading critical section" you must acquire a Semaphore. 
+ * To enter the "writing section" you must first ensure there is nobody else in the reading section AND there is nobody else in the "writing section" (i.e. you have exclusive access).
+ *
+ * NOTE that there are by default only 4 permits for reading (but it could be increased to any number >1 at the cost of performance).
+ *
+ * If a reader (client that wishes to read a file i.e. open it in READ mode) requests access and there is a permit available, it will be given read access to the file.
+ * This is designed in such a way that if another has opened the file for writing, it will have acquired all the permits (see below). 
+ * In this way you can only have one writer or any number of readers at any time.
+ *
+ * To enter the "writing critical section" you must both acquire all permits (ensures that nobody is reading) 
+ * and you get the lock (ensures nobody else is writing).
+ *
+ * If a writer requests access to a file that is being read by n clients, it will have to wait until all of them release the semaphores and n becomes 0.
+ * Then the reader acquires all semaphore permits effectively blocking read access for the file. The writer also needs to get the lock for the file, which in turn ensures there is only one client writing at a time (prevents multiple writes to the same file).
+ *
+ * Fairness is ensured by instantiating the `Semaphores` and `ReentrantLocks` with parameter `fair` set to `true`. 
+ * They are implemented in such a way that the underlying queue gives equal (i.e. fair amount of time to all clients to requested files). 
+ *
+ * Additionally, attempting to open a file in a mode undefined by the specification (i.e. the assessment instructions) results in failure which here is basically returning of the empty `Optional`.
+ *
  * @author nl253
  */
 
@@ -70,7 +96,10 @@ public class MyFileServer implements FileServer {
     public void close(final File file) {
         for (int i = 0; i < files.size(); i++) {
             final File focus = files.get(i);
-            if (focus.equals(file)) files.set(i, new File(file.filename(), file.read(), Mode.CLOSED));
+            if (focus.filename().equals(file.filename())) {
+                files.set(i, new File(file.filename(), file.read(), Mode.CLOSED));
+                return;
+            }
         }
     }
 
