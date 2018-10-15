e69ca9d8700d31e6e0b72068a17b8ec9d2a93d7a
nl253 <norbertlogiewa96@gmail.com>
2018-10-15 18:38:14 +0100

    Refactor

--- Client.java
+++ Client.java
@@ -1,17 +1,17 @@
-import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static java.lang.System.setProperty;
-import static java.text.MessageFormat.*;
+import static java.text.MessageFormat.format;
 
 /**
- *
  * TODO add comments.
  *
  * @author nl253
@@ -23,48 +23,88 @@ public class Client extends Thread {
     private static final int ASCII_UPPER_BOUND = 126;
     private static final int NO_ACTIONS = 20;
     private static final int NO_CLIENTS = 20;
+    private static final int MAX_RAND_TEXT_LEN = 2000;
+    private static final int MIN_RAND_TEXT_LEN = 100;
     private static AtomicInteger nextId = new AtomicInteger(0);
 
     private final Logger log = Logger.getAnonymousLogger();
     private final int id;
-    private final Server server;
+    private final FileServer server;
 
-    @override
+
+    @Override
     public void run() {
-        for (int i = 0; i < NO_ACTIONS; i++) {
+        IntStream.range(0, NO_ACTIONS).forEach((int i) -> {
             final boolean write = ThreadLocalRandom.current().nextBoolean();
             final String fileName = randFileName(new ArrayList<>(server.availableFiles()));
-            log.info(format("client no {0} about to perform {1}{2} action, more specifically {3} on file \"{4}\"", id, i, (i == 1) ? "st" : ((i == 2) ? "nd" : ((i == 3) ? "rd" : "st")), write ? "write" : "read", fileName));
-            server.open(fileName, write ? Mode.READWRITEABLE : Mode.READABLE);
-        }
+            log.info(format("{0} about to perform {1}{2} action, more specifically {3} on file \"{4}\"", toString(), i, i == 1 ? "st" : i == 2 ? "nd" : i == 3 ? "rd" : "st", write ? "write" : "read", fileName));
+            final Optional<File> file = server
+                    .open(fileName, write ? Mode.READWRITEABLE : Mode.READABLE);
+            file.ifPresent((final File f) -> {
+                switch (f.mode()) {
+                    case READABLE:
+                        f.read();
+                        log.info(format("{} successfully read file \"{}\"", toString(), f
+                                .filename()));
+                        break;
+                    case READWRITEABLE:
+                        final String text = randomText();
+                        f.write(text);
+                        log.info(format("{} successfully wrote {} bytes to file \"{}\"", toString(), text
+                                .length(), f.filename()));
+                        break;
+                }
+            });
+        });
     }
 
-    public Client(final Server fileServer) {
+    public Client(final FileServer fileServer) {
         id = nextId.incrementAndGet();
         server = fileServer;
-        log.info("client no " + id + " created");
+        log.info(format("client no {0} created", id));
     }
 
-    private String randFileName(final List<String> allFiles) {
-        return allFiles.get(ThreadLocalRandom.current().nextInt(allFiles.size()));
+    private static String randFileName(final List<String> allFiles) {
+        return allFiles
+                .get(ThreadLocalRandom.current().nextInt(allFiles.size()));
     }
 
-    public String randomText() {
-        return IntStream.range(0, 1000)
-                .mapToObj(i -> String.valueOf(ASCII_LOWER_BOUND + ThreadLocalRandom.current().nextInt(ASCII_UPPER_BOUND - ASCII_LOWER_BOUND)))
-                .collect(Collectors.joining())
-                .toString();
+    private static String randomText() {
+
+        final int randTextLen = MIN_RAND_TEXT_LEN + ThreadLocalRandom.current()
+                .nextInt(MAX_RAND_TEXT_LEN);
+
+        final StringBuilder builder = new StringBuilder(randTextLen + 1);
+
+        for (int i = 0; i < randTextLen; i++) {
+            final char c = (char) (ASCII_LOWER_BOUND + ThreadLocalRandom
+                    .current().nextInt(ASCII_UPPER_BOUND - ASCII_LOWER_BOUND));
+            builder.append(c);
+        }
+        return builder.toString();
     }
 
     public static void main(final String... args) {
         setProperty("java.util.logging.SimpleFormatter.format", "%1$tH:%1$tM:%1$tS %4$-6s %2$s %5$s%6$s%n");
-        final MyFileServer server = new MyFileServer();
-        final List<Client> clients = new LinkedList<>();
+        final FileServer server = new MyFileServer();
+        final Deque<Client> clients = new LinkedList<>();
         for (int i = 0; i < NO_CLIENTS; i++) {
             final Client c = new Client(server);
-            clients.append(c);
+            clients.addLast(c);
             c.start();
         }
-        clients.forEach(Client::join);
+        clients.forEach((final Client client) -> {
+            try {
+                client.join();
+            } catch (final InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
+
+    @Override
+    public final String toString() {
+        return format("Client {0}", id);
     }
 }
--- MyFileServer.java
+++ MyFileServer.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -7,31 +10,48 @@ import java.util.stream.Collectors;
 import static java.text.MessageFormat.format;
 
 /**
- * This implementation of `FileServer` ensures fairness and avoids races. It does so in the following ways.
- * A `ReentrantLock` AND a `Semaphore` is created for every file.
- *
- * See `files`, there is always a single lock and a single semaphore for the ith file stored in locks in the ith index and semaphores in the ith index respectively.
- *
- * There are essentially two nested "critical sections" with the "writing section" being inside the "reading section". 
- * To enter the "reading critical section" you must acquire a Semaphore. 
- * To enter the "writing section" you must first ensure there is nobody else in the reading section AND there is nobody else in the "writing section" (i.e. you have exclusive access).
- *
- * NOTE that there are by default only 4 permits for reading (but it could be increased to any number >1 at the cost of performance).
- *
- * If a reader (client that wishes to read a file i.e. open it in READ mode) requests access and there is a permit available, it will be given read access to the file.
- * This is designed in such a way that if another has opened the file for writing, it will have acquired all the permits (see below). 
- * In this way you can only have one writer or any number of readers at any time.
- *
- * To enter the "writing critical section" you must both acquire all permits (ensures that nobody is reading) 
- * and you get the lock (ensures nobody else is writing).
- *
- * If a writer requests access to a file that is being read by n clients, it will have to wait until all of them release the semaphores and n becomes 0.
- * Then the reader acquires all semaphore permits effectively blocking read access for the file. The writer also needs to get the lock for the file, which in turn ensures there is only one client writing at a time (prevents multiple writes to the same file).
- *
- * Fairness is ensured by instantiating the `Semaphores` and `ReentrantLocks` with parameter `fair` set to `true`. 
- * They are implemented in such a way that the underlying queue gives equal (i.e. fair amount of time to all clients to requested files). 
- *
- * Additionally, attempting to open a file in a mode undefined by the specification (i.e. the assessment instructions) results in failure which here is basically returning of the empty `Optional`.
+ * This implementation of `FileServer` ensures fairness and avoids races. It
+ * does so in the following ways. A `ReentrantLock` AND a `Semaphore` is created
+ * for every file.
+ * <p>
+ * See `files`, there is always a single lock and a single semaphore for the ith
+ * file stored in locks in the ith index and semaphores in the ith index
+ * respectively.
+ * <p>
+ * There are essentially two nested "critical sections" with the "writing
+ * section" being inside the "reading section". To enter the "reading critical
+ * section" you must acquire a Semaphore. To enter the "writing section" you
+ * must first ensure there is nobody else in the reading section AND there is
+ * nobody else in the "writing section" (i.e. you have exclusive access).
+ * <p>
+ * NOTE that there are by default only 4 permits for reading (but it could be
+ * increased to any number >1 at the cost of performance).
+ * <p>
+ * If a reader (client that wishes to read a file i.e. open it in READ mode)
+ * requests access and there is a permit available, it will be given read access
+ * to the file. This is designed in such a way that if another has opened the
+ * file for writing, it will have acquired all the permits (see below). In this
+ * way you can only have one writer or any number of readers at any time.
+ * <p>
+ * To enter the "writing critical section" you must both acquire all permits
+ * (ensures that nobody is reading) and you get the lock (ensures nobody else is
+ * writing).
+ * <p>
+ * If a writer requests access to a file that is being read by n clients, it
+ * will have to wait until all of them release the semaphores and n becomes 0.
+ * Then the reader acquires all semaphore permits effectively blocking read
+ * access for the file. The writer also needs to get the lock for the file,
+ * which in turn ensures there is only one client writing at a time (prevents
+ * multiple writes to the same file).
+ * <p>
+ * Fairness is ensured by instantiating the `Semaphores` and `ReentrantLocks`
+ * with parameter `fair` set to `true`. They are implemented in such a way that
+ * the underlying queue gives equal (i.e. fair amount of time to all clients to
+ * requested files).
+ * <p>
+ * Additionally, attempting to open a file in a mode undefined by the
+ * specification (i.e. the assessment instructions) results in failure which
+ * here is basically returning of the empty `Optional`.
  *
  * @author nl253
  */
@@ -45,15 +65,17 @@ public class MyFileServer implements FileServer {
     private static final int NO_READERS = 4;
 
     @Override
-    public void create(final String filename, final String content) {
+    public final void create(final String filename, final String content) {
         files.add(new File(filename, content, Mode.CLOSED));
         locks.add(new ReentrantLock(true));
         semaphores.add(new Semaphore(NO_READERS, true));
     }
 
     @Override
-    public Optional<File> open(final String filename, final Mode mode) {
-        assert mode.equals(Mode.READABLE) || mode.equals(Mode.READWRITEABLE) : format("Mode must be {0} or {1}", Mode.READABLE.toString(), Mode.READWRITEABLE);
+    public final Optional<File> open(final String filename, final Mode mode) {
+        assert mode.equals(Mode.READABLE) || mode
+                .equals(Mode.READWRITEABLE) : format("Mode must be {0} or {1}", Mode.READABLE
+                .toString(), Mode.READWRITEABLE);
         synchronized (files) {
             for (int i = 0; i < files.size(); i++) {
                 final File focus = files.get(i);
@@ -61,7 +83,8 @@ public class MyFileServer implements FileServer {
                     case READABLE:
                         try {
                             semaphores.get(i).acquire();
-                            final File openedFile = new File(focus.filename(), focus.read(), Mode.READABLE);
+                            final File openedFile = new File(focus.filename(), focus
+                                    .read(), Mode.READABLE);
                             files.set(i, openedFile);
                             return Optional.ofNullable(openedFile);
                         } catch (final InterruptedException e) {
@@ -76,7 +99,8 @@ public class MyFileServer implements FileServer {
                             semaphores.get(i).acquire(NO_READERS);
                             // wait until nobody is writing
                             locks.get(i).lock();
-                            final File openedFile = new File(focus.filename(), focus.read(), Mode.READWRITEABLE);
+                            final File openedFile = new File(focus.filename(), focus
+                                    .read(), Mode.READWRITEABLE);
                             files.set(i, openedFile);
                             return Optional.ofNullable(openedFile);
                         } catch (final InterruptedException e) {
@@ -93,23 +117,30 @@ public class MyFileServer implements FileServer {
     }
 
     @Override
-    public void close(final File file) {
+    public final void close(final File file) {
         for (int i = 0; i < files.size(); i++) {
             final File focus = files.get(i);
             if (focus.filename().equals(file.filename())) {
-                files.set(i, new File(file.filename(), file.read(), Mode.CLOSED));
+                files.set(i, new File(file.filename(), file
+                        .read(), Mode.CLOSED));
                 return;
             }
         }
     }
 
     @Override
-    public Mode fileStatus(final String filename) {
-        return files.stream().filter(f -> f.filename().equals(filename)).findFirst().get().mode();
+    public final Mode fileStatus(final String filename) {
+        return files.stream().filter(f -> f.filename().equals(filename))
+                .findFirst().get().mode();
     }
 
     @Override
-    public Set<String> availableFiles() {
+    public final Set<String> availableFiles() {
         return files.stream().map(File::filename).collect(Collectors.toSet());
+    }
+
+    @Override
+    public final String toString() {
+        return format("MyFileServer with {} files", files.size());
     }
 }
